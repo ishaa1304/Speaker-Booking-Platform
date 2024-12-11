43b020c184fd49d043be44044bbff10098ca41b8
Isha Deo <76875905+ishaa1304@users.noreply.github.com>
2024-12-11 23:52:34 +0530

    Add files via upload

---
+++ backend/Controllers/authController.js
@@ -0,0 +1,6 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+
---
+++ backend/server.js
@@ -0,0 +1,44 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import speakersRoutes from "./routes/speakers.js";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// Register routes
+app.use('/api/users', userRoutes);
+app.use("/api/speakers", speakersRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend server!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
